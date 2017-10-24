13ab5fc69470d3d3715d93b2dce233a4e9a16b7c
Muhammed Demirci <demirci.muhammedd@gmail.com>
2017-10-24 18:13:07 +0300

    tema mvc ye geçirildi

---
+++ Odi/ClientMvc/Controllers/SettingsChangePasswordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientMvc.Controllers
+{
+    public class SettingsChangePasswordController : Controller
+    {
+        // GET: SettingsChangePassword
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Odi/ClientMvc/Controllers/SettingsRequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientMvc.Controllers
+{
+    public class SettingsRequestController : Controller
+    {
+        // GET: SettingsRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
